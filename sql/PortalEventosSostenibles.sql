0085b790f7024c412d63010a3b3103ee487c6bb7
GregoryMA23 <150656760+GregoryMA23@users.noreply.github.com>
2025-05-24 23:28:41 +0200

    creacion deL SCRIPT de sql

---
+++ sql/PortalEventosSostenibles.sql
@@ -0,0 +1,49 @@
+USE master;
+GO
+IF DB_ID (N'PortalEventosSostenibles') IS NOT NULL
+DROP DATABASE PortalEventosSostenibles;
+CREATE DATABASE PortalEventosSostenibles;
+USE PortalEventosSostenibles;
+
+CREATE TABLE Usuario (
+    correo VARCHAR(100) PRIMARY KEY ,
+    nombre VARCHAR(50) NOT NULL,
+    contraseña VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE Organizadores (
+    nombre VARCHAR(100) PRIMARY KEY ,
+    contacto VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Evento (
+    nombre VARCHAR(100) PRIMARY KEY,
+    fecha DATE NOT NULL,
+    duracion INT NOT NULL,
+    ubicacion VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE se_inscribe (
+    usuario VARCHAR(100),
+    evento VARCHAR(100),
+    fecha date,
+    PRIMARY KEY (usuario, evento),
+    FOREIGN KEY (usuario) REFERENCES Usuario(correo),
+    FOREIGN KEY (evento) REFERENCES Evento(nombre)
+)
+
+CREATE TABLE participa (
+    usuario VARCHAR(100),
+    evento VARCHAR(100),
+    fecha date,
+    PRIMARY KEY (usuario, evento),
+    FOREIGN KEY (usuario) REFERENCES Usuario(correo),
+    FOREIGN KEY (evento) REFERENCES Evento(nombre)
+)
+
+CREATE TABLE Localizacion (
+    direccion VARCHAR(255) PRIMARY KEY,
+    evento VARCHAR(100),
+    tipo VARCHAR(50) NOT NULL,
+    FOREIGN KEY (evento) REFERENCES Evento(nombre)
+)
